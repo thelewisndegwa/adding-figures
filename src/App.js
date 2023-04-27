8a0d83634969fa1e122f22687c4894c732c483f0
thelewisndegwa <lewiskanyi77@gmail.com>
2023-04-27 17:35:13 +0300

    done

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{ useState }from 'react'
+
 
 function App() {
+
+
+const [Value1,setValue1]=useState(0)
+const [Value2,setValue2]=useState(0)
+const [Answer,setAnswer]=useState(0)
+
+
+const handleAdd = (e) => {
+    e.preventDefault();
+    setAnswer(Number(Value1) + Number(Value2));
+
+  
+  // function handleAdd(value1,value2) {
+  //    let answer = (value1 + value2)
+  //     }
+    }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <form onSubmit={handleAdd}>
+    <label>
+      First Value
+      <input type="number"  value={Value1} onChange={(e) => setValue1(e.target.value)} />
+    </label>
+    <label>
+     Second value
+      <input type="number"  value={Value2}  onChange={(e) => setValue2(e.target.value)} />
+    </label>
+   
+    <button type="submit">Add Values</button>
+  </form>
+
+<p>The answer is {Answer} </p>
+</>
   );
 }
 
---
+++ src/testtest.js
@@ -0,0 +1,27 @@
+// import React ,{ useState, useEffect }from 'react'
+//  function Form(){
+//     const [Num1,setNum1]=useState("")
+//     const [Num2,setNum2]=useState("")
+//     const [Sum,setSum]=useState("")
+//     const handleChange = (e) => {
+//         e.preventDefault();
+//         setSum(Num1+Num2);
+//     };
+//   return (
+//     <div>
+//         <form>
+//         <label>
+//         1ST number:
+//         <input type="number" value={Num1} onChange={e.target.value} required />
+//       </label>
+//       <label>
+//       2ND number:
+//         <input type="number" value={Num2} onChange={e.target.value} required />
+//       </label>
+//       <button onClick={}>ADD</button>
+// <p>Ans = {Sum}</p>
+//     </form>
+//     </div>
+//   )
+// };
+// export default Form;
